3793e256641af9a04f82f87060fb7ac709b4efbb
aneesawan34 <aneesawan34@yahoo.com>
2020-02-21 01:22:55 +0500

    react.memo basic code for learning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import InputChild from "./InputChild";
+import InputChild from "./ChildComponent";
 
 class App extends React.Component {
   constructor() {
